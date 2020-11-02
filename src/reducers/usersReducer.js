ceae2f99e96591b7adfe1a9cff8a276c96382601
Oscar <oscar.a.chavez.jr@gmail.com>
2020-11-02 01:10:30 -0700

    first commit

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,10 @@
+import postsReducer from "./postsReducer";
+
+export default (state = [], action) => {
+  switch(action.type) {
+    case 'FETCH_USER':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
